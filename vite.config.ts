461550eee23a478c9978be600fed70f001301b5c
GabrielMontel <gabriel.montel@iut-dijon.u-bourgogne.fr>
2026-05-22 21:03:45 +0200

    modif site config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  // Cette ligne indique à Vite de chercher les fichiers CSS/JS à la racine du site
-  base: '/', 
+  base: '/',
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://gabriel-montel.vercel.app/',
+      hostname: 'https://gabriel-montel.vercel.app/', // Remplace par ton URL si besoin
     }),
   ],
+  build: {
+    // On force Vite à utiliser esbuild à la place de lightningcss pour minifier le CSS
+    cssMinify: 'esbuild' 
+  }
 })
