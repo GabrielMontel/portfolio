b4e1ebfeccef8ec98b24dbe2f60da9a99c2609c8
GabrielMontel <gabriel.montel@iut-dijon.u-bourgogne.fr>
2026-05-22 21:11:24 +0200

    Fix final : Tailwind v4 compile parfaitement

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
+import tailwindcss from '@tailwindcss/vite' // <-- ON IMPORTE LE PLUGIN ICI
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
+    tailwindcss(), // <-- ON ACTIVE TAILWIND ICI
     sitemap({
-      hostname: 'https://gabriel-montel.vercel.app/', // Remplace par ton URL si besoin
+      hostname: 'https://gabriel-montel.vercel.app/',
     }),
   ],
   build: {
-    // On force Vite à utiliser esbuild à la place de lightningcss pour minifier le CSS
-    cssMinify: 'esbuild' 
+    cssMinify: true
   }
 })
