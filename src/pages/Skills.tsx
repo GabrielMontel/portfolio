057231f7aeaa2bb66bea857eb63712b20fee0024
Gabriel MONTEL <gm876820@iut21.u-bourgogne.fr>
2026-05-26 13:59:16 +0200

    modif skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Cpu, Layers, Monitor, Server, Palette } from "lucide-react";
+import { Layers, Monitor, Server, Palette } from "lucide-react";
 
 export default function Skills() {
   const categories = [
